3f51420378b7c022a1ea19a01fe7d51c7041427a
hoalex <svt75@hotmail.com>
2019-02-18 17:02:09 +0100

    Interface and navigation has been created.

---
+++ MiaWouf/Controller/FormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormViewController.swift
+//  MiaWouf
+//
+//  Created by Alex on 18/02/2019.
+//  Copyright © 2019 Alexandre Holet. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
